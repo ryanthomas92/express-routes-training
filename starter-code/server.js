64fbc2a5bf1741ec85fb11e31e4a515178ca2dc1
ryanthomas92 <rthoma11@villanova.edu>
2016-12-13 18:08:15 -0800

    pick a number finished

--- starter-code/server.js
+++ starter-code/server.js
@@ -22,13 +22,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // ROUTES
 // Root Route
-app.get
+app.get('/', function (request, response) {
+  response.sendFile('views/index.html' , { root : __dirname});
+});
 
 // Gallery View Route
-
+app.get('/art-gallery', function (request, response) {
+  response.sendFile('views/art-gallery.html' , { root : __dirname});
+});
 
 // The Number Guessing Game
+var targetNumber = 7;
 
+app.get('/pick-a-number', function(request, response){
+  var num = parseInt(request.query.number);
+  if (num === targetNumber){
+    response.send('Nailed it!');
+  } else if (num > targetNumber){
+    response.send('Too High!');
+  } else if (num < targetNumber) {
+    response.send('Too Low');
+  } else {
+    console.log('Something odd happened! target:', targetNumber, typeof(targetNumber),'num:', num, typeof(num));
+    response.send('Your guess is beyond compare!');
+  }
+});
+
+app.post('/pick-a-number', function(request, response){
+  targetNumber = parseInt(request.body.number);
+  response.status(200).send('Number updated successfully!');
+});
 
 // Gallery
 
